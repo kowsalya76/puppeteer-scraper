f700f5f3d6612f8201c8176cf6ebcf6805a58ae5
Kowsalya Mannepalli <75980622+kowsalya76@users.noreply.github.com>
2025-08-05 12:42:21 +0530

    Update render-build.sh

--- render-build.sh
+++ render-build.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env bash
 set -e
 
-# Define Puppeteer cache dir
-export PUPPETEER_CACHE_DIR=$PWD/.cache/puppeteer-cache
+apt-get update
+apt-get install -y wget gnupg curl
 
-mkdir -p $PUPPETEER_CACHE_DIR
+# Add Google Chrome key and repo
+wget -q -O - https://dl.google.com/linux/linux_signing_key.pub | apt-key add -
+sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" > /etc/apt/sources.list.d/google-chrome.list'
 
-# Restore Puppeteer cache if exists
-if [ -d "$PUPPETEER_CACHE_DIR" ]; then
-  echo "Restoring Puppeteer cache..."
-  cp -r $PUPPETEER_CACHE_DIR $XDG_CACHE_HOME
-fi
+apt-get update
+apt-get install -y google-chrome-stable
 
-# Install node_modules and Puppeteer Chromium browsers
+# Install node modules
 npm install
-
-# Save Puppeteer cache for future builds
-cp -r $XDG_CACHE_HOME/puppeteer $PUPPETEER_CACHE_DIR
